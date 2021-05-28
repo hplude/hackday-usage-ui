b25937f2c368bec2126e42ddd207652567f825c3
Hunter Plude <hunterplude@sf-mbp-hp.lan>
2021-05-28 14:41:19 -0700

    working version!

--- routes/usage.js
+++ routes/usage.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var usage = require('../lib/usage');
 
 router.post('/', function(req, res, next) {
-  res.send('sup');
-});
+  usage(req.body).then((file) =>{
+  	res.sendFile(file[0])
+  });
+  // res.send('fsdf')
+  // .then((file) => {
+  	// res.send(file);
+  });
+  
+// });
 
 /* GET users listing. */
 router.get('/total-requests', function(req, res, next) {
