0a6e143b1689419d6bf52c7dd18de35439819958
Michael Davis <michael.davis@algolia.com>
2021-05-28 12:30:23 -0500

    wiring up usage post route

--- routes/usage.js
+++ routes/usage.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/', function(req, res, next) {
+  res.send('sup');
+});
+
 /* GET users listing. */
 router.get('/total-requests', function(req, res, next) {
   res.send('hello ');
